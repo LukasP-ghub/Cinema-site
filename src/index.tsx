93cfe6231b8cf6172c7c14f6800fa5846d37f9aa
Lukasz Pelczar <lukasxxz@o2.pl>
2021-08-22 21:35:18 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "tailwindcss/tailwind.css";
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode><div className="text-red-400">Alleluja</div></React.StrictMode>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv').config({ path: './.env' });;
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader']
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    }),
+  ],
+}
